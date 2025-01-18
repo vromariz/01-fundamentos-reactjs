3a3906f342a90a6df0d2c695cdc5325982a69a02
vinicius <viniciusrfscabral@gmail.com>
2025-01-17 22:02:32 -0300

    side bar completa mas nada no vite

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,4 +1,6 @@
-import styles from './Sidebar.module.css'
+import {PencilLine} from 'phosphor-react'
+import styles from './Sidebar.module.css';
+
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
@@ -6,13 +8,17 @@ export function Sidebar() {
                 className={styles.sidebar}
                 src="https://images.unsplash.com/photo-1736210811075-7509d5023a4a?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
+
             <div className={styles.profile}>
+                <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/138325339?s=400&u=56a44e82ae6083603336319e005dafa8e76902c2&v=4'/>
+                
                 <strong>Diego fernando</strong>
                 <span>Web developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size = {20}/>
                     Editar seu perfi
                 </a>
             </footer>
