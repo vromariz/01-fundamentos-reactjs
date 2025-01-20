89344add6783e9e48cc2657d2dad4ac1afe9f8fc
vinicius <viniciusrfscabral@gmail.com>
2025-01-20 00:54:37 -0300

    ajuste para caber em diferentes telas

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -7,7 +7,8 @@ export function Sidebar() {
         <aside className={styles.sidebar}>
             <img
                 className={styles.sidebar}
-                src="https://images.unsplash.com/photo-1736210811075-7509d5023a4a?q=50&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://images.unsplash.com/photo-1736210811075-7509d5023a4a?q=400&w=644&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                
             />
 
             <div className={styles.profile}>
