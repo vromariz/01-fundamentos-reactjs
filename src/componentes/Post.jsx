c4fe1485cd6d9b935ffa73fcdc8d3f935e620699
vinicius <viniciusrfscabral@gmail.com>
2025-01-19 21:48:50 -0300

    post

--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -2,7 +2,7 @@ import styles from './Post.module.css';
 
 export function Post() {
     return (
-        <article class={styles.Post}>
+        <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src='https://avatars.githubusercontent.com/u/138325339?s=400&u=56a44e82ae6083603336319e005dafa8e76902c2&v=4' />
@@ -11,6 +11,7 @@ export function Post() {
                         <span>student</span>
                     </div>
                 </div>
+                
                 <time title="11 de maio as 8:20" data time="2022-05-11 08:13:38">Publicado ha 1h</time>
             </header>
 
@@ -18,8 +19,12 @@ export function Post() {
                 <p>Fala galeraa 👋</p>
 
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p> 👉 <a href="">jane.design/doctorcare</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto </a>{' '}
+                    <a href="">#nlw </a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
             </div>
 
         </article>
